350273f1ae87389446edd834707a0f3bcfd4760b
Johannes Valtonen <jvaltonen@gmail.com>
2015-05-12 13:16:40 +0300

    Created the project structure

---
+++ SpecFlowNullReference/NullReference.feature
@@ -0,0 +1,6 @@
+﻿Feature: NullReference
+
+Scenario: Set and retrieve null value
+	Given I set null value to ScenarioContext
+	When I get value from ScenarioContext
+	Then the value should be null
---
+++ SpecFlowNullReference/NullReferenceSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowNullReference
+{
+    [Binding]
+    public class NullReferenceSteps
+    {
+        private const string Key = "SomeKey";
+        private object value;
+        private NullReferenceException exception;
+
+        [Given(@"I set null value to ScenarioContext")]
+        public void GivenISetNullValueToScenarioContext()
+        {
+            ScenarioContext.Current.Set<object>(null, Key);
+        }
+
+        [When(@"I get value from ScenarioContext")]
+        public void WhenIGetValueFromScenarioContext()
+        {
+            value = ScenarioContext.Current.Get<object>(Key);
+        }
+
+        [Then(@"the value should be null")]
+        public void ThenNullReferenceExceptionIsThrown()
+        {
+            Assert.IsNull(value);
+        }
+    }
+}
